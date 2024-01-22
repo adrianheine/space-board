4a2438ffd675a810d761ede0753ed3a28acd21fd
Adrian Heine <mail@adrianheine.de>
2024-01-22 23:58:27 +0100

    Dynamically get widget names for the sidebar
    This is probably not gonna work out in the long run.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 
 const Widget = {
+  inject: ["widgetName"],
   props: ["name"],
-  template: `<li><slot /></li>`
+  template: `<li><slot /></li>`,
+  created() {
+    this.widgetName(this.name)
+  }
 }
 
 const Image = {
@@ -11,31 +15,32 @@ const Image = {
 }
 
 const Dashboard = {
-  props: ["widgets"],
-  template: `
-  <article>
-  <ul class=dashboard>
-    <slot />
-  </ul>
-  </article>
-<aside>
-  <nav>
-    <a v-for="widget in widgets">{{ widget.name }}</a>
-  </nav>
-</aside>
-`}
-
-createApp({
   data() {
     return {
       widgets: [
-        { name: "Wetter" },
-        { name: "Termine" },
-        { name: "Webcam" },
       ]
     }
-  }
-})
+  },
+
+  provide() {
+    const self = this;
+    return { widgetName(name) { self.widgets.push({ name }) } }
+  },
+
+  template: `
+    <article>
+      <ul class=dashboard>
+        <slot />
+      </ul>
+    </article>
+    <aside>
+      <nav>
+        <a v-for="widget in widgets">{{ widget.name }}</a>
+      </nav>
+    </aside>
+`}
+
+createApp()
 .component('Widget', Widget)
 .component('Image', Image)
 .component('Dashboard', Dashboard)
